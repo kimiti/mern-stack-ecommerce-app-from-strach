c7fb0c3ed0d335c97587ed2e3e82d691ca1a1548
kimiti <kimitikevin@gmail.com>
2020-11-11 12:56:54 +0300

    added dummy products and routs in react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { Container } from "react-bootstrap";
 // components
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <main className="py-3">
-          <Container>
-            <HomeScreen />
-          </Container>
-        </main>
-        <Footer />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Header />
+          <main className="py-3">
+            <Container>
+              <Route exact path="/" component={HomeScreen} />
+              <Route exact path="/product/:id" component={ProductScreen} />
+            </Container>
+          </main>
+          <Footer />
+        </header>
+      </div>
+    </Router>
   );
 };
 
