aaab791d0396a51a80f56b0ec5f9f613e8a76ef0
kimiti <kimitikevin@gmail.com>
2020-11-11 11:33:54 +0300

    added homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+import { Container } from "react-bootstrap";
+// components
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Welcome to proshop</h3>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <HomeScreen />
+          </Container>
+        </main>
+        <Footer />
       </header>
     </div>
   );
